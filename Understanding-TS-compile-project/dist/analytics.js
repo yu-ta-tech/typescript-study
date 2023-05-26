6fc4d846ec50380904522d45b52ca6d5bf8339b0
Yuta Ueda <yuta8722@icloud.com>
2023-05-26 22:34:20 +0900

    TypeScriptの設定とコンパイラ、デバッグ

---
+++ Understanding-TS-compile-project/dist/analytics.js
@@ -0,0 +1,10 @@
+"use strict";
+let logged;
+function sendAnalytics(data) {
+    console.log(data);
+    logged = true;
+    logged = "Max";
+    console.log(logged);
+}
+sendAnalytics("The data");
+//# sourceMappingURL=analytics.js.map
