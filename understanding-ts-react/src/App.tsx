2bc82b643fda5cb7978050378d4dd497c49bfced
Yuta Ueda <yuta8722@icloud.com>
2023-06-09 22:36:03 +0900

    React × TypeScript

---
+++ understanding-ts-react/src/App.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+// import { Route } from "react-router-dom";
+
+import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
+import { Todo } from "./todo.model";
+
+// function App() {
+//   return <div className="App"></div>;
+// }
+
+const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const todoAddHandler = (text: string) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      { id: Math.random().toString(), text: text },
+    ]);
+  };
+
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== todoId));
+  };
+
+  return (
+    <div className="App">
+      <NewTodo onAddTodo={todoAddHandler} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler} />
+    </div>
+  );
+};
+
+export default App;
