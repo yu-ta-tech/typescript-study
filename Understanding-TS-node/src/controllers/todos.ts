f30e34a110dd61e1a41156a1eff07c0a56dc2e9a
Yuta Ueda <yuta8722@icloud.com>
2023-06-12 18:46:10 +0900

    Node.js + Express + TypeScript

---
+++ Understanding-TS-node/src/controllers/todos.ts
@@ -0,0 +1,49 @@
+import { RequestHandler } from "express";
+
+import { Todo } from "../models/todo";
+
+const TODOS: Todo[] = [];
+
+export const createTodo: RequestHandler = (req, res, next) => {
+  const text = (req.body as { text: string }).text;
+  const newTodo = new Todo(Math.random().toString(), text);
+
+  TODOS.push(newTodo);
+
+  res
+    .status(201)
+    .json({ message: "TODOを作成しました。", createTodo: newTodo });
+};
+
+export const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ todos: TODOS });
+};
+
+export const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+  const updateText = (req.body as { text: string }).text;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error("対象のTODOが見つかりませんでした。");
+  }
+
+  TODOS[todoIndex] = new Todo(todoId, updateText);
+
+  res.json({ message: "更新しました。", updateTodo: TODOS[todoIndex] });
+};
+
+export const deleteTodo: RequestHandler = (req, res, next) => {
+  const todoId = req.params.id;
+
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex < 0) {
+    throw new Error("対象のTODOが見つかりませんでした。");
+  }
+
+  TODOS.splice(todoIndex, 1);
+
+  res.json({ message: "TODOを削除しました。" });
+};
